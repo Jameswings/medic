c841915fb28a123b336da43c6436af8e3c2b4ce2
James Cheung <zhiqiang.zhang@dinglicom.com>
2013-04-19 05:33:39 +0800

    demo update

---
+++ WebContent/dating/src/data/Calendars.js
@@ -0,0 +1,18 @@
+Ext.define('Ext.calendar.data.Calendars', {
+    statics: {
+        getData: function(){
+            return {
+                "calendars":[{
+                    "id":    1,
+                    "title": "Urgent"
+                },{
+                    "id":    2,
+                    "title": "Important"
+                },{
+                    "id":    3,
+                    "title": "Normal"
+                }]
+            };    
+        }
+    }
+});
